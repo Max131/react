7fe47820d8ac4e0f3b5f0a927fe6e7b428c5215b
Mario <max_cable@hotmail.com>
2025-10-15 01:08:20 -0600

    feat(object-literal.ts): añadir un nuevo archivo que define un objeto persona

---
+++ 01-reinforcement/src/bases/03-object-literal.ts
@@ -0,0 +1,15 @@
+const firstName: string = "John";
+const lasName: string = "Doe";
+const age: number = 30;
+
+const person: {
+  firstName: string;
+  lasName: string;
+  age: number;
+} = {
+  firstName,
+  lasName,
+  age,
+};
+
+console.log(person);
