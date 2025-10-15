8bbab4759e172b497c6dc5cd7172f975ece01e77
Mario <31197372+Max131@users.noreply.github.com>
2025-10-15 16:40:12 -0600

    feat(functions.ts): añadir funciones getUser y getUser2 para obtener usuarios

--- 01-reinforcement/src/bases/05-functions.ts
+++ 01-reinforcement/src/bases/05-functions.ts
@@ -8,3 +8,20 @@ const message = greet("Alice");
 const message2 = greet2("Bob");
 
 console.log(message, message2);
+
+function getUser() {
+  return {
+    uid: 1,
+    username: "Alice",
+  };
+}
+
+const getUser2 = (): object => ({
+  uid: 2,
+  username: "Bob",
+});
+
+const user = getUser();
+const user2 = getUser2();
+
+console.log(user, user2);
