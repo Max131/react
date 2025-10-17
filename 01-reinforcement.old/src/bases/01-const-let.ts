8bc470eba1f7aef6f0a4f062638a7a5336d8d8a9
Mario <me@mabreumx.com>
2025-10-16 19:06:56 -0600

    chore: eliminar el proyecto de refuerzo antiguo y sus archivos asociados

--- 01-reinforcement.old/src/bases/01-const-let.ts
+++
@@ -1,7 +0,0 @@
-const firstName: string = "John";
-const lastName: string = "Doe";
-let age: number = 30;
-
-age = 36;
-
-console.log({ firstName, lastName, age });
--- 01-reinforcement.old/src/bases/02-template-strings.ts
+++
@@ -1,6 +0,0 @@
-const firstName: string = "John";
-const lastName: string = "O'Doe";
-
-console.log(`Hello, ${firstName} ${lastName}!`);
-
-
