45134043eb5934010e8e8242b1ed5b7328c42407
Mario <31197372+Max131@users.noreply.github.com>
2025-10-21 10:26:31 -0600

    feat(FirstStepsApp): crear un nuevo componente FirstStepsApp con contenido refactor(main.tsx): reemplazar el contenido estático por el componente FirstStepsApp

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,13 @@
+export function FirstStepsApp() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>Consectetur molestiae deserunt.</p>
+      <button>Click me!</button>
+
+      <div>
+        <h2>Hello, earth!</h2>
+      </div>
+    </>
+  );
+}
