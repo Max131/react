cea95123eff8467d31607a387fe3a2deaeec2949
Mario <max_cable@hotmail.com>
2025-10-21 13:28:27 -0600

    feat(ItemCounter.tsx): implementar contador de ítems con estado local y botones de suma/resta

--- 02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,4 +1,4 @@
-import type { CSSProperties } from "react";
+import { useState, type CSSProperties } from "react";
 
 interface Props {
   name: string;
@@ -13,17 +13,24 @@ const style: CSSProperties = {
   width: "min(100%, 400px)",
 };
 
-export const ItemCounter = ({ name, quantity = 0 }: Props) => {
-  const handeClick = () => {
-    console.log(`Clicked ${name}`);
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+  const [count, setCount] = useState(quantity);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubstract = () => {
+    if (count === 0) return;
+    setCount(count - 1);
   };
 
   return (
     <section style={style}>
       <span style={{ flex: 1 }}>{name}</span>
-      <button onClick={handeClick}>-1</button>
-      <span>{quantity}</span>
-      <button>+1</button>
+      <button onClick={handleSubstract}>-1</button>
+      <span>{count}</span>
+      <button onClick={handleAdd}>+1</button>
     </section>
   );
 };
