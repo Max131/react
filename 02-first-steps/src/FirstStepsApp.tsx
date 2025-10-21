a2658d211ce95cf62750211cd4392486c96d22d0
Mario <31197372+Max131@users.noreply.github.com>
2025-10-21 12:02:57 -0600

    feat(FirstStepsApp.tsx): añadir interfaz ItemInCart y lista de productos en el carrito refactor(FirstStepsApp.tsx): reemplazar instancias de ItemCounter por un mapeo dinámico

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,5 +1,18 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  { productName: "XBOX 360", quantity: 2 },
+  { productName: "Pro Controller", quantity: 1 },
+  { productName: "Castlevania IV", quantity: 3 },
+  { productName: "The Legend of Zelda: Ocarina of Time", quantity: 1 },
+  { productName: "Super Mario 64", quantity: 4 },
+];
+
 export function FirstStepsApp() {
   return (
     <>
@@ -11,9 +24,9 @@ export function FirstStepsApp() {
         <h2>Hello, earth!</h2>
       </div> */}
       <h1>Shopping Cart</h1>
-      <ItemCounter name="XBOX 360" quantity={2} />
-      <ItemCounter name="Pro Controller" quantity={1} />
-      <ItemCounter name="Castlevania IV" quantity={3} />
+      {itemsInCart.map(({ productName, quantity }) => (
+        <ItemCounter key={productName} name={productName} quantity={quantity} />
+      ))}
     </>
   );
 }
