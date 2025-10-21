51f8e2dc033abf9551be718f4b61acecd9c21fad
Mario <31197372+Max131@users.noreply.github.com>
2025-10-21 10:40:16 -0600

    feat(MyAwesomeApp.tsx): refactorizar el componente para usar variables y objetos refactor(MyAwesomeApp.tsx): mejorar la presentación de datos en el componente

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,24 @@
+const name = "John";
+const lastName = "Doe";
+
+const figures = ["square", "circle", "triangle", "rectangle"];
+const isActive = true;
+
+const address = {
+  street: "123 Main St",
+  city: "Anytown",
+  country: "USA",
+};
+
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>John</h1>
-      <h3>Doe</h3>
+      <h1>{name}</h1>
+      <h3>{lastName}</h3>
+      <p> {figures.join(", ")} </p>
+      <p> {5 + 6} </p>
+      <h1>{isActive ? "Active" : "Inactive"}</h1>
+      <p>{JSON.stringify(address, null, 2)}</p>
     </>
   );
 }
