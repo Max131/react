5817a4d98ed7413d9d31787681563a2b3b4d7968
Mario <31197372+Max131@users.noreply.github.com>
2025-10-28 12:21:21 -0600

    test(MyAwesomeApp.test.tsx): mejorar la prueba para MyAwesomeApp refactor(MyAwesomeApp.test.tsx): descomentar y reorganizar el código de prueba

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -1,10 +1,23 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MyAwesomeApp } from "./MyAwesomeApp";
 
 describe("MyAwesomeApp", () => {
   test("Should render firstName and lastName", () => {
-    const { container } = render(<MyAwesomeApp />);
-    console.log(container.innerHTML);
+    // const { container } = render(<MyAwesomeApp />);
+    render(<MyAwesomeApp />);
+    // screen.debug();
+
+    const h1 = screen.getByRole("heading", {
+      level: 1,
+    });
+
+    console.log(h1.innerHTML);
+
+    // const h1 = container.querySelector("h1");
+    // const h3 = container.querySelector("h3");
+    //
+    // expect(h1?.innerHTML).toBe("John");
+    // expect(h3?.innerHTML).toContain("Doe");
   });
 });
