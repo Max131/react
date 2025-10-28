f5e192a0b1b14d4477f12e08f1f4e978580d0d6c
Mario <31197372+Max131@users.noreply.github.com>
2025-10-28 11:41:48 -0600

    feat(tests): añadir pruebas para el componente MyAwesomeApp

---
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -0,0 +1,10 @@
+import { describe, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MyAwesomeApp } from "./MyAwesomeApp";
+
+describe("MyAwesomeApp", () => {
+  test("Should render firstName and lastName", () => {
+    const { container } = render(<MyAwesomeApp />);
+    console.log(container.innerHTML);
+  });
+});
