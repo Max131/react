6a5eaf86238669c3850996082c5bc66e0d2c24a2
Mario <31197372+Max131@users.noreply.github.com>
2025-11-03 14:14:23 -0600

    test(ItemCounter.test.tsx): añadir pruebas para incrementar y decrementar el contador test(ItemCounter.test.tsx): evitar que el contador baje de 0 al presionar -1

--- 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { ItemCounter } from "./ItemCounter";
 
@@ -17,5 +17,45 @@ describe("ItemCounter", () => {
     render(<ItemCounter name={name} quantity={quantity} />);
 
     expect(screen.getByText(quantity)).toBeDefined();
+  });
+
+  test("should increase count when +1 button is pressed", () => {
+    const name = "Control de Nintendo";
+    const quantity = 1;
+
+    render(<ItemCounter name={name} quantity={quantity} />);
+
+    const [, buttonAdd] = screen.getAllByRole("button");
+
+    fireEvent.click(buttonAdd);
+
+    expect(screen.getByText("2")).toBeDefined();
+  });
+
+  test("should decrease count when -1 button is pressed", () => {
+    const name = "Control de Nintendo";
+    const quantity = 5;
+
+    render(<ItemCounter name={name} quantity={quantity} />);
+
+    const [buttonSubstract] = screen.getAllByRole("button");
+
+    fireEvent.click(buttonSubstract);
+
+    expect(screen.getByText("4")).toBeDefined();
+  });
+
+  test("should not decrease count when -1 button is pressed and quantity is 1", () => {
+    const name = "Control de Nintendo";
+    const quantity = 1;
+
+    render(<ItemCounter name={name} quantity={quantity} />);
+
+    const [buttonSubstract] = screen.getAllByRole("button");
+
+    fireEvent.click(buttonSubstract);
+    fireEvent.click(buttonSubstract);
+
+    expect(screen.getByText("0")).toBeDefined();
   });
 });
