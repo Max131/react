3d66e24605476fcbb96366778f02865cf5de5d57
Mario <31197372+Max131@users.noreply.github.com>
2025-11-05 15:24:36 -0600

    test(FirstStepsApp.test.tsx): añadir pruebas para el componente FirstStepsApp test(FirstStepsApp.test.tsx): simular el componente ItemCounter en las pruebas test(FirstStepsApp.test.tsx): verificar el número correcto de componentes ItemCounter

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,17 +1,27 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { FirstStepsApp } from "./FirstStepsApp";
 
+vi.mock("./shopping-cart/ItemCounter", () => ({
+  ItemCounter: () => <div data-testid="ItemCounter"></div>,
+}));
+
 describe("FirstStepsApp", () => {
+  test("Should render Shopping Cart", () => {
+    render(<FirstStepsApp />);
+    expect(FirstStepsApp).toBeDefined();
+  });
+
   test("Should be snapshot", () => {
     const { container } = render(<FirstStepsApp />);
     expect(container).toMatchSnapshot();
-
-    screen.debug();
   });
 
-  test("Should render Shopping Cart title", () => { });
-  render(<FirstStepsApp />);
+  test("Should render the correct number of ItemCounter components", () => {
+    render(<FirstStepsApp />);
 
-  expect(FirstStepsApp).toBeDefined();
+    const itemCounters = screen.getAllByTestId("ItemCounter");
+
+    expect(itemCounters.length).toBe(5);
+  });
 });
