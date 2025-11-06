f3bd6a5836e3be0f906739644799597bdfffb40d
Mario <max_cable@hotmail.com>
2025-11-06 14:59:34 -0600

    feat(gifs-app): agregar componente GifsApp y estilos CSS para la app

---
+++ 03-gifs-app/src/GifsApp.tsx
@@ -0,0 +1,10 @@
+export const GifsApp = () => {
+  return (
+    <>
+      <div>
+        <h1>Buscador de Gifs</h1>
+        <p>Descubre y comparte el gif perfecto</p>
+      </div>
+    </>
+  );
+};
