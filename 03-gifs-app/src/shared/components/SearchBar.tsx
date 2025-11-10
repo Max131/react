989cf9a6aafe66687e4234a1c142fd43cbf95708
Mario <31197372+Max131@users.noreply.github.com>
2025-11-10 14:34:19 -0600

    feat(GifsApp): añadir función handleSearch para manejar consultas de búsqueda feat(SearchBar): permitir pasar onQuery como prop y manejar eventos de entrada

--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 interface Props {
   placeholder?: string;
+  onQuery?: (query: string) => void;
 }
-export const SearchBar = ({ placeholder = "Buscar" }: Props) => {
+export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    onQuery?.(query);
+    // setQuery("");
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <search className="search-container">
-      <input type="text" placeholder={placeholder} />
-      <button>Buscar</button>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onInput={(event) => setQuery(event.target.value)}
+        onKeyDown={(event) => {
+          handleKeyDown(event);
+        }}
+      />
+      <button onClick={handleSearch}>Buscar</button>
     </search>
   );
 };
