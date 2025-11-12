112e9e680051173b74c4612871ba24e8a08eaf3b
Mario <me@mabreumx.com>
2025-11-11 20:11:01 -0600

    feat(SearchBar): añadir debounce a la función de búsqueda con useEffect
    Se añade un efecto que retrasa la ejecución de la función onQuery
    para evitar llamadas excesivas mientras el usuario escribe en el
    campo de búsqueda. Esto mejora la eficiencia y la experiencia
    del usuario al reducir la cantidad de consultas realizadas.

--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   placeholder?: string;
@@ -6,6 +6,16 @@ interface Props {
 }
 export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
   const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onQuery?.(query);
+    }, 700);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [query, onQuery]);
 
   const handleSearch = () => {
     onQuery?.(query);
