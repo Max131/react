a212302a21e26cb2446deb4ed1cdd9cb4ca95efb
Mario <31197372+Max131@users.noreply.github.com>
2025-11-27 13:43:06 -0600

    refactor(get-gifs-by-query.action.ts): renombrar la función y mejorar el tipo de retorno feat(get-gifs-by-query.action.ts): mapear la respuesta para devolver un array de Gif[]

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.ts
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import type { GiphyResponse } from "../interfaces/giphy.response";
+import type { Gif } from "../interfaces/gif.interface";
 
-export const getGifsByQueryAction = async (query: string) => {
+export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
   const response = await axios.get<GiphyResponse>(
     "https://api.giphy.com/v1/gifs/search",
     {
@@ -14,5 +15,11 @@ export const getGifsByQueryAction = async (query: string) => {
     },
   );
 
-  console.log(response.data);
+  return response.data.data.map((gif) => ({
+    id: gif.id,
+    title: gif.title,
+    url: gif.images.original.url,
+    width: Number(gif.images.original.width),
+    height: Number(gif.images.original.height),
+  }));
 };
